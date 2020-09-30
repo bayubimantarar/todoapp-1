ffb7400380bcd84059844af1bb7ad89c9ba7a6fb
Rizky Irmawan <rzkyirmwn@gmail.com>
2020-10-01 06:04:22 +0700

    Added js math methods

--- todo/app.js
+++ todo/app.js
@@ -60,3 +60,14 @@ function statement() {
 
 	console.log('The value of z is: ' + z + '.');
 }
+
+function maths() {
+	let x = 10.5;
+	let y = 15.0;
+	let z = -5;
+
+	console.log('Math.round returns the value of x rounded to its nearest integer: ' + Math.round(x));
+	console.log('Math.pow returns the value of x to the power of y: ' + Math.pow(x, y));
+	console.log('Math.sqrt returns the square root of x: ' + Math.sqrt(y));
+	console.log('Math.abs returns the absolute (positive) value of x: ' + Math.sqrt(z));
+}
